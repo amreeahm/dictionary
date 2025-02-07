6bc83602a0e6e1b7124df7b0f0b4ccb9207d52a3
amreeahm <khanamrees@gmail.com>
2025-02-07 14:11:19 -0800

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 
+const arr = [{ word: "React", meaning: "A JavaScript library for building user interfaces." }, { word: "Component", meaning: "A reusable building block in React." }, { word: "State", meaning: "An object that stores data for a component." }]
+
 function App() {
+  const [input, setInput] = useState("");
+  const [meaning, setMeaning] = useState("")
+
+  const search = () => {
+    const word = input.slice(0, 1).toUpperCase() + input.slice(1);
+    const found = arr.find((item) => item.word === word);
+
+    if (found) {
+      setMeaning(found.meaning);
+    } else {
+      setMeaning("Word not found in the dictionary.");
+    }
+
+    setInput("");
+  }
+
+
   return (
-    <div className="App">
-      Add
+    <div >
+      <h1>Dictionary App</h1>
+      <input placeholder="Search for a word..." value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={search}>Search</button>
+      <h3>Definition:</h3>
+      <p>{meaning}</p>
     </div>
   );
 }
